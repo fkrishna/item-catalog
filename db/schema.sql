df9ce87c39e48893547fbe75d71abbcda6fb9f96
Krishna Farvil <fkrishna.dev@gmail.com>
2019-02-25 15:44:43 -0800

    Feat: Create database schema

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE categories (
+	id serial PRIMARY KEY NOT NULL,
+ 	title varchar(255) NOT NULL
+);
+
+CREATE TABLE items (
+	id serial PRIMARY KEY NOT NULL,
+ 	name varchar(255) NOT NULL,
+ 	description text,
+ 	post_date timestamp NOT NULL,
+ 	category_id integer NOT NULL REFERENCES categories(id)
+);
